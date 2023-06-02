837e0b66e9b9f06856a82ed4252f9d27b803434d
urimJ <ufrl128@dgu.ac.kr>
2023-06-03 04:27:48 +0900

    MOD

--- simple-search-engine/simple-search-engine/read_file.c
+++ simple-search-engine/simple-search-engine/read_file.c
@@ -3,24 +3,28 @@
 void read_file(char* file_name, int index) {
 
 	FILE* file;
-	Element element;
+	Word word_original;
 	
 	int line = 0;
 
-	file = fopen(file_name, "r");
-
-	element.doc.file_num = index;
+	file = fopen( file_name, "r");
 
 	while (1) {
-		fscanf(file, "%s ", element.word_original);
+		if (file == NULL)
+		{
+			printf("%s 화일 오픈 불가\n", file_name);    //화면에 오류 표시
+			
+			break;
+		}
+		fscanf(file, "%s ", word_original.word_original);
 		if (fgetc(file) != '\n') {
 			line++;
 		}
 		
-		strcpy(element.word_original, refine_word(element.word_original)); // 단어만 저장
-		strcpy(element.word_original, _strlwr(element.word)); // 소문자로 저장
+		strcpy(word_original.word_original, refine_word(word_original.word_original)); // 단어만 저장
+		strcpy(word_original.word_original, _strlwr(word_original.word_original)); // 소문자로 저장
 		//hash_insert(element.word, element.doc.file_num, line); // 해시테이블에 저장
-		printf("%s ", element.word);
+		printf("%s ", word_original.word_original);
 
 		if (feof(file)) break;
 	}
