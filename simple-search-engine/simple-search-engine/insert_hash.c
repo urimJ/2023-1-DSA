77344a308ed0b49bca2ee44f15c2bcf7bb1dfaa9
urimJ <ufrl128@dgu.ac.kr>
2023-06-06 22:23:34 +0900

    MOD hash_insert

--- simple-search-engine/simple-search-engine/insert_hash.c
+++ simple-search-engine/simple-search-engine/insert_hash.c
@@ -4,15 +4,14 @@ void insert_hash(char* word, int file_num, int n_row) {
 	int hash_value = hash(word);
 	int BSTkey = 0;
 	struct Node* head_node;
-	struct Node* new_node;
+
 
 	if (hash_table[hash_value].word != NULL) {		// 해시 밸류에 해당하는 테이블이 비었을 경우
+
 		//BST의 노드를 만들어서 값을 저장
 
-		struct Node* new_node;
-
-		hash_table[hash_value].key = hash_value + n_row;
-		BSTkey = hash_value + n_row;
+		hash_table[hash_value].key = hash_value;
+		BSTkey = hash_value + file_num;
 		//strcpy(hash_table[hash_value].word, word);	// 해당 단어 복사
 		//hash_table[hash_value].doc.file_num = file_num;
 		//hash_table[hash_value].doc.sentence = sentence;
@@ -22,9 +21,16 @@ void insert_hash(char* word, int file_num, int n_row) {
 
 
 	}
-	else {											// 해시 밸류에 해당하는 테이블이 이미 차 있는 경우
+	else {							// 해시 밸류에 해당하는 테이블이 이미 차 있는 경우
 		BSTkey = hash_value + n_row;
 		insertNode(hash_table[hash_value].node, BSTkey, word, file_num, n_row);
 	}
+	for (int i = 0; i < TABLE_SIZE; i++) {
+		total_num_of_indexed_words = 0;
+		if (hash_table[i].node != NULL) {
+			total_num_of_indexed_words++;
+		}
+	}
+
 
 }
